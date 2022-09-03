96d9c74d273443b4171b797d2e56749877f48402
Sam <sam.finestone@gmail.com>
2022-09-03 16:44:47 +0100

    updated with option 1 and 2 enabled

---
+++ run_dual_causal_both.sh
@@ -0,0 +1,3 @@
+python train_temporal_baseline.py --config configs/felix_temporal_config_both --version sum_fusion --causal True &
+python train_temporal_baseline.py --config configs/felix_temporal_config_both --version conv3d_fusion --causal True &
+python train_temporal_baseline.py --config configs/felix_temporal_config_both --version causal_fusion --causal True
