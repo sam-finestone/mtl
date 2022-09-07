f6632c30452037b21b102be0a5e2f277a23c02d7
Sam <sam.finestone@gmail.com>
2022-09-07 14:18:31 +0100

    adding bash scripts

---
+++ slow_fast_seg_sum.sh
@@ -0,0 +1,1 @@
+python train_temporal_baseline.py --config configs/felix_temporal_config_seg --version sum_fusion --causal True
---
+++ slowfast_conv3d_multitask.sh
@@ -0,0 +1,1 @@
+python train_temporal_baseline.py --config configs/felix_temporal_config_both --version conv3d_fusion --causal True 
