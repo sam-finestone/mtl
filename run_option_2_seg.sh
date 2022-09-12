6005a6ba6b111e8ddda3096c66e465be333671e8
Sam Finestone <45223347+sam-finestone@users.noreply.github.com>
2022-09-12 16:03:14 +0100

    deleting unnecessary files

--- run_option_2_seg.sh
+++
@@ -1,3 +0,0 @@
-python train_temporal_baseline.py --config configs/felix_temporal_config_seg --version sum_fusion &
-python train_temporal_baseline.py --config configs/felix_temporal_config_seg --version convnet_fusion &
-python train_temporal_baseline.py --config configs/felix_temporal_config_seg --version conv3d_fusion 
