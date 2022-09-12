87adb355185163ab743dd8bc4455e763575152fb
Sam Finestone <45223347+sam-finestone@users.noreply.github.com>
2022-09-12 16:03:31 +0100

    Delete run_dual_causal_both.sh

--- run_dual_causal_both.sh
+++
@@ -1,3 +0,0 @@
-python train_temporal_baseline.py --config configs/felix_temporal_config_both --version sum_fusion --causal True &
-python train_temporal_baseline.py --config configs/felix_temporal_config_both --version conv3d_fusion --causal True &
-python train_temporal_baseline.py --config configs/felix_temporal_config_both --version causal_fusion --causal True
